98c9601bf6597f4c7091ae2cb9d9f4090a44876d
Zmining <dieimissom2007@gmail.com>
2024-08-08 16:37:44 -0300

    changed some id's

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,5 +1,4 @@
 // Definir as variáveis
-
 const senhaDigitada = document.getElementById("password");
 const maiusculas = document.getElementById("maius");
 const minusculas = document.getElementById("minus");
@@ -13,29 +12,33 @@ function geraSenha(num) {
     var senha = '';
 
     // Verificar se foi marcado pelo menos uma das opções
-    if (maiusculas.checked == false && minusculas.checked == false && numeros.checked == false && simbolos.checked == false) {
+    if (!maiusculas.checked && !minusculas.checked && !numeros.checked && !simbolos.checked) {
         senhaDigitada.innerHTML = 'Marque pelo menos uma das opções';
-        return a
+        return a;
     }
-    
+
     // Somar as opções selecionadas
-    if (maius.checked == true) {
+    if (maiusculas.checked) {
         for (var i = 65; i <= 90; i++) { // Letras maiúsculas
-        alfabeto += String.fromCharCode(i);
-    } }
-    if (minus.checked == true) {
+            alfabeto += String.fromCharCode(i);
+        }
+    }
+    if (minusculas.checked) {
         for (var i = 97; i <= 122; i++) { // Letras minúsculas
-        alfabeto += String.fromCharCode(i);
-    } }
-    if (numeros.checked == true) {
+            alfabeto += String.fromCharCode(i);
+        }
+    }
+    if (numeros.checked) {
         for (var i = 48; i <= 57; i++) { // Números 0-9
-        alfabeto += String.fromCharCode(i);
-    }}
-    if (simbolos.checked == true) {
-        for( var i = 33; i <= 38; i++) { // Simbolos
-        alfabeto += String.fromCharCode(i)
-    }}
-    
+            alfabeto += String.fromCharCode(i);
+        }
+    }
+    if (simbolos.checked) {
+        for (var i = 33; i <= 47; i++) { // Símbolos
+            alfabeto += String.fromCharCode(i);
+        }
+    }
+
     // Gerar a senha
     for (var i = 0; i < num; i++) {
         var r = Math.floor(Math.random() * alfabeto.length);
@@ -45,21 +48,21 @@ function geraSenha(num) {
     return senha;
 }
 
-// Chamar a função através do botão de gerar 
-let passwordDigits = document.getElementById("input");
-let gerarSenha = document.getElementById("gerarSenha");
+// Chamar a função através do botão de gerar
+let passwordDigits = document.getElementById("input"); 
+let gerarSenha = document.getElementById("gerarSenha"); 
 gerarSenha.addEventListener("click", () => {
     let tamanhoSenha = parseInt(passwordDigits.value);
     if (isNaN(tamanhoSenha) || tamanhoSenha <= 0) {
         tamanhoSenha = 5;
     } else if (tamanhoSenha > 15) {
         senhaDigitada.innerHTML = "Senha muito grande, tente novamente. (1-15)";
-        return
+        return;
     }
-    
+
     let senha = geraSenha(tamanhoSenha); // Pega o return da função
 
-    // Sistema de repeticão para evitar senhas repetidas
+    // Sistema de repetição para evitar senhas repetidas
     let counter = 0;
     while (senhas.includes(senha) && counter < 50) {
         senha = geraSenha(tamanhoSenha);
@@ -71,27 +74,27 @@ gerarSenha.addEventListener("click", () => {
         senhas.push(senha);
         senhaDigitada.innerHTML = senha;
     }
-    
+
     // Verificar se está funcionando
     console.log(senhas);
-    console.log(minus.value)
+    console.log(minusculas.value);
 });
 
-// Sistema e limpar
-let limpar = document.getElementById("limpar")
+// Sistema de limpar
+let limpar = document.getElementById("limpar");
 limpar.addEventListener("click", () => {
     senhaDigitada.innerHTML = "";
-})
+});
 
 // Copiar a senha
 const copiar = document.getElementById('copiar');
 copiar.addEventListener('click', copyToClipboard);
 async function copyToClipboard() {
-  try {
-    const text = senhaDigitada.innerHTML;
-    await navigator.clipboard.writeText(text);
-
-} catch (err) {
-    console.error('Erro ao copiar: ', err);
-} 
+    try {
+        const text = senhaDigitada.innerHTML;
+        await navigator.clipboard.writeText(text);
+        alert("Senha copiada com sucesso!");
+    } catch (err) {
+        alert('Erro ao copiar: ', err);
+    }
 }
